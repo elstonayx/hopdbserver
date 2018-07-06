8e507ef852235a08956c488381afeb0bb99d8ae8
Elston Aw <elston.ayx@u.nus.edu>
2018-07-06 22:29:52 +0800

    V2.6.0 - Added Google Places API Functionality to fetch URL links for photos

--- helper/formatData.js
+++ helper/formatData.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
+const gplaces = require("./gplaces");
 const cafeModel = require("./../models/cafeModel");
 
-packageCafeModel = rawCafeData => {
+packageCafeModel = async rawCafeData => {
+  var photoArray = await gplaces.searchCafe(rawCafeData.response.venue.name);
   var package = new cafeModel.Cafe({
     name: rawCafeData.response.venue.name,
     fsVenueId: rawCafeData.response.venue.id,
     url: rawCafeData.response.venue.canonicalUrl,
+    images: photoArray,
     contactNo: rawCafeData.response.venue.contact.formattedPhone,
     address: [
       rawCafeData.response.venue.location.formattedAddress[0] + ",",
