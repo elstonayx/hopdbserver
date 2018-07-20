28b58ab04f102c3fcf7e003a245d693d4351a301
Elston Aw <elston.ayx@u.nus.edu>
2018-07-20 23:52:49 +0800

    V3.1.2 Fixed return status bug

--- helper/status.js
+++ helper/status.js
@@ -1,22 +1,22 @@
 var response = (statusCode, message) => {
   if (statusCode >= 500) {
-    return JSON.stringify({
+    return {
       success: false,
       statusCode: statusCode,
       response: message
-    });
+    };
   } else if (statusCode >= 400) {
-    return JSON.stringify({
+    return {
       success: false,
       statusCode: statusCode,
       response: message
-    });
+    };
   } else if (statusCode >= 300) {
-    return JSON.stringify({
+    return {
       success: true,
       statusCode: statusCode,
       response: message
-    });
+    };
   } else if (statusCode >= 200) {
     return {
       success: true,
